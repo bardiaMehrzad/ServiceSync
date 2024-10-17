9ba7b497b59454caed2aa850b42ac19ccff49a5a
bardia1978 <bardiam.mehrzad@gmail.com>
2024-10-17 15:22:51 -0700

    Created Admin Page (NOUI)

---
+++ my-app/src/AdminCalander.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const AdminCalendar = () => {
+    return (
+        <div>
+            <h1>Admin Calendar</h1>
+        </div>
+    );
+};
+
+export default AdminCalendar;
