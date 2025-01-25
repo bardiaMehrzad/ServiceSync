e2a4a10833b6368c8d06386acd49015ce8cb57cc
Bilal Baloch <bbaloch2@csus.edu>
2025-01-25 08:50:42 -0800

    added firebase auth. assuming one for both admin and employee

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -13,6 +13,31 @@ import LoginPage from "./pages/LoginPage";
 import CompanyCreation from "./pages/CompanyCreation";
 import AdminJobPage from "./pages/AdminJobPage";
 import EmployeeCalendar from "./pages/EmployeeCalendar";
+
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyC-zHokpPV316sdixgDhPizAgz4h_m1U84",
+  authDomain: "fir-for-auth-web-app.firebaseapp.com",
+  projectId: "fir-for-auth-web-app",
+  storageBucket: "fir-for-auth-web-app.firebasestorage.app",
+  messagingSenderId: "79542475148",
+  appId: "1:79542475148:web:2e835ba094dbf469979ea9",
+  measurementId: "G-CVX4XY78EL"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+// Initialize Firebase Authentication and get a reference to the service
+const auth = getAuth(app);
+
 function App() {
   return (
     <Router>
